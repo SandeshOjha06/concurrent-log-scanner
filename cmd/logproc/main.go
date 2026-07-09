af289045a3337630d8f14bec986fce736897adfe
Sandesh <sandeshojha70@gmail.com>
2026-07-09 22:06:55 +0530

    feat: add scaffolding code
    Signed-off-by: Sandesh <sandeshojha70@gmail.com>

--- cmd/logproc/main.go
+++ cmd/logproc/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/SandeshOjha06/go-systems-practice"
+)
+
+func main(){
+	
+	dirPath := flag.String("dir", "./logs", "dirrctory containing log files")
+
+	target := flag.String("word", "ERROR", "Counter of specific words")
+
+	workers := flag.String("workers", 4, "Number of concurrent goroutinrs")
+
+	flag.Parse()
+
+	if *dirPath == "" || *target == "" {
+		log.Fatal("Both -dir and -word flags are required")
+	}
+
+	startTime := time.Now()
+
+	totalCount, err := logproc.RunEngine(*dirPath, *target, *workers) 
+
+	if err != nil {
+		log.Fatal("Execution failed: %v", err)
+	}
+
+
+
+}
