8604579681e3c5d94cefe0fd7e1de1893f474f6e
Sandesh <sandeshojha70@gmail.com>
2026-07-13 22:03:56 +0530

    feat: add channel

--- internal/logproc/worker.go
+++ internal/logproc/worker.go
@@ -1,14 +1,49 @@
 package logproc
 
-import(
+import (
 	"fmt"
+	"io/fs" 
+	"path/filepath"
+	"sync"
 )
 
+var wg sync.WaitGroup
 
-func RunEngine(dirpath string, target string, workers int) (int, error) {
-	fmt.Printf("worker received, dirpath: %s, target: %s, workers: %d\n", dirpath, target, workers)
+func RunEngine(dirPath string, target string, workers int) (int, error) {
+	jobs := make(chan string, 100)
 
+	// take file info
+	go func() {
+		err := filepath.WalkDir(dirPath, func(path string, d fs.DirEntry, err error) error {
+			if err != nil { 
+				return err
+			} 
 
-	return  0, nil
-	
+			if !d.IsDir() {
+				jobs <- path
+			}
+
+			return nil
+		}) 
+
+		if err != nil {
+			fmt.Printf("Error working directory: %v\n", err)
+		}
+
+		close(jobs)
+	}()
+
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			for path := range jobs { 
+				fmt.Printf("Worker in path: %s\n", path) 
+			}
+		}() 
+	}
+
+	return 0, nil
 }
